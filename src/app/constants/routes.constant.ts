204314706ae3ad47817e16541b538cefec8193a8
Plamen Koynov <pl.koynov@gmail.com>
2022-06-26 11:13:50 +0300

    Use routing constants

---
+++ src/app/constants/routes.constant.ts
@@ -0,0 +1,15 @@
+export const ROUTES = {
+    LEARN: {
+        SETTINGS: 'learn',
+        GAME: 'game',
+    },
+    WRITE: {
+        SETTINGS: 'write',
+        EASY_GAME: 'easy',
+        HARD_GAME: 'hard',
+    },
+    SETTINGS: {
+        LIST: 'settings',
+        EDIT: 'edit',
+    },
+}
